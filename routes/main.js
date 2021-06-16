eaeccb2a6030e10353246355c57a5e164bcb4497
MrJaymun <alexerm71@mail.ru>
2021-06-16 15:47:37 +0300

    view of tests to pass with filters and searching

--- routes/main.js
+++ routes/main.js
@@ -36,6 +36,24 @@ router.post('/second-level', async function(req, res, next) {
 });
 
 
+router.post('/testToPass', async function(req, res, next) {
+
+    await dataBase.sequelize.query(`SELECT A.TEST_ID, A.TEST_CATEGORY_ID, B.TEST_CATEGORY_NAME, A.TEST_NAME, COUNT(C.*) AS COUNTER FROM TESTS A
+        JOIN TEST_CATEGORIES B
+        ON A.TEST_CATEGORY_ID = B.TEST_CATEGORY_ID
+        JOIN QUESTIONS C
+        ON A.TEST_ID = C.TEST_ID
+        WHERE A.TEST_STATUS_ID = 2
+        GROUP BY A.TEST_ID, A.TEST_CATEGORY_ID, B.TEST_CATEGORY_NAME, A.TEST_NAME`
+    ).then(result=>{
+        console.log(result[0])
+        return res.status(201).json({status: "1", result: result[0]})
+    }).catch((error)=> {
+        return res.status(201).json({status: "2"})
+    })
+
+
+});
 
 
 
